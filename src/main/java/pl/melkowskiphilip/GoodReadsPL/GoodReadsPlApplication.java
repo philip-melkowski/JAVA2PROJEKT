4044cb199bdf9a86dbcf38a77fdeecb6ea07c51f
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2025-10-24 18:05:00 +0200

    stworzenie repo

---
+++ src/main/java/pl/melkowskiphilip/GoodReadsPL/GoodReadsPlApplication.java
@@ -0,0 +1,13 @@
+package pl.melkowskiphilip.GoodReadsPL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodReadsPlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodReadsPlApplication.class, args);
+	}
+
+}
