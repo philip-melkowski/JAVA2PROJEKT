1f6a8cdc1b76ba68101d94165f64618549a3d7bf
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2025-12-23 13:28:04 +0100

    dodanie drugiego testu dla activateAccount

--- src/test/java/pl/melkowskiphilip/GoodReadsPL/service/ActivationTokenServiceTest.java
+++ src/test/java/pl/melkowskiphilip/GoodReadsPL/service/ActivationTokenServiceTest.java
@@ -1,6 +1,5 @@
 package pl.melkowskiphilip.GoodReadsPL.service;
 
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,11 +7,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.melkowskiphilip.GoodReadsPL.entity.ActivationToken;
 import pl.melkowskiphilip.GoodReadsPL.entity.User;
+import pl.melkowskiphilip.GoodReadsPL.exception.custom.InvalidActivationTokenException;
 import pl.melkowskiphilip.GoodReadsPL.repository.ActivationTokenRepository;
 import pl.melkowskiphilip.GoodReadsPL.repository.UserRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -103,10 +105,67 @@ public class ActivationTokenServiceTest {
         // sprawdzamy, czy save zostało wywołane raz
         verify(activationTokenRepository, times(1)).save(any(ActivationToken.class));
 
+    }
+    // koniec testów dla CreateTokenForUser()
+
+    // testy dla activateAccount(token)
+    @Test
+    void shouldActivateAccount()
+    {
+
+        // arrange
+
+        User user = new User();
+        user.setId(1L);
+        user.setEnabled(false);
+
+        String token = "VALID_TOKEN";
+        ActivationToken tokenToActivate = new ActivationToken();
+        tokenToActivate.setToken(token);
+        tokenToActivate.setExpiresAt(LocalDateTime.now().plusDays(1)); // token ma być aktywny
+        tokenToActivate.setUser(user);
+
+        when(activationTokenRepository.findByToken(token)).thenReturn(Optional.of(tokenToActivate));
+
+        // act
+
+        activationTokenService.activateAccount(token);
+
+        // assert
+
+        assert user.isEnabled(); // czy user enabled
+        verify(userRepository, times(1)).save(user);
+
+        verify(activationTokenRepository, times(1)).delete(tokenToActivate);
+
+    }
+
+    @Test
+    void tokenDoesntExist()
+    {
+
+        // arrange
+        String token = "NONEXISTENT_TOKEN";
+
+        when(activationTokenRepository.findByToken(token)).thenReturn(Optional.empty());
+
+        // act + assert
+
+        // sprawdź, czy service.activate rzuca błąd
+        assertThrows(InvalidActivationTokenException.class, () -> activationTokenService.activateAccount(token));
+
+
+        verify(userRepository, never()).save(any(User.class));
+        verify(activationTokenRepository, never()).delete(any(ActivationToken.class));
 
 
     }
 
+    @Test
+    void tokenExpired()
+    {
+
+    }
 
 
 }
