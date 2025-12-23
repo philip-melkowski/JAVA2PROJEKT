7b2bd78beb1385366bb3dc605a860a688031abd9
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2025-12-23 11:20:03 +0100

    koretka shouldCreateTokenForUserTest

--- src/test/java/pl/melkowskiphilip/GoodReadsPL/service/ActivationTokenServiceTest.java
+++ src/test/java/pl/melkowskiphilip/GoodReadsPL/service/ActivationTokenServiceTest.java
@@ -1,15 +1,20 @@
 package pl.melkowskiphilip.GoodReadsPL.service;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.melkowskiphilip.GoodReadsPL.entity.ActivationToken;
+import pl.melkowskiphilip.GoodReadsPL.entity.User;
 import pl.melkowskiphilip.GoodReadsPL.repository.ActivationTokenRepository;
 import pl.melkowskiphilip.GoodReadsPL.repository.UserRepository;
 
 import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ActivationTokenServiceTest {
@@ -62,7 +67,46 @@ public class ActivationTokenServiceTest {
 
     // koniec testow isTokenValid()
 
-    // testy
+
+    // testy dla createTokenForUser()
+
+    @Test
+    void shouldCreateTokenForUser()
+    {
+        // arrange
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("example@example.com");
+
+
+        // przygotowanie tokenu
+        ActivationToken savedToken = new ActivationToken();
+        savedToken.setToken("UUID");
+        savedToken.setUser(user);
+        savedToken.setCreatedAt(LocalDateTime.now());
+        savedToken.setExpiresAt(LocalDateTime.now().plusDays(7));
+
+        // obsługa funkcji save w atrapie repo
+        // jeśli wywolane zostanie save w repo to niech atrapa zwroci savedToken
+        // potrzebne, bo createTokenForUser zwraca repo.save()
+        when(activationTokenRepository.save(any(ActivationToken.class)))
+                .thenReturn(savedToken);
+
+        ActivationToken result = activationTokenService.createTokenForUser(user);
+
+        assert result.getToken() != null;
+        assert result.getUser().getId().equals(1L);
+        assert result.getCreatedAt() != null;
+        assert result.getExpiresAt().equals(result.getCreatedAt().plusDays(7));
+
+
+        // sprawdzamy, czy save zostało wywołane raz
+        verify(activationTokenRepository, times(1)).save(any(ActivationToken.class));
+
+
+
+    }
+
 
 
 }
