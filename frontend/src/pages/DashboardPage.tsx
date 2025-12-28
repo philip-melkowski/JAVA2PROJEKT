ddefb0dd7ae4714b0b9df03909dcf1cd21c2b23d
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2025-12-28 19:45:34 +0100

    dodanie przycisku wyloguj na /dashboard

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,5 +1,28 @@
-
+import {useAuth} from "../auth/AuthContext.tsx";
+import {Box, Button, Typography} from "@mui/material";
 
 export default function DashboardPage() {
-    return <div>Dashboard</div>
+
+    const {logout} = useAuth();
+
+    return (
+        <Box
+            p={3}
+        >
+            <Button
+                sx = {{padding: 10, width: 50, height: 20, color: "blue"}}
+                variant="contained"
+                color="secondary"
+                align="right"
+                onClick={logout}
+            >Wyloguj się</Button>
+            <Typography variant="h5" align="center" mb={2}>
+                Jesteś zalogowany!
+            </Typography>
+
+
+
+        </Box>
+    )
+
 }
