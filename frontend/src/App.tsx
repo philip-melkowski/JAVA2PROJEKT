75324a6b460fe7103b1a17ec1f271888768aeb8c
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2025-12-28 19:31:32 +0100

    dodanie routingu miedzy /login i /dashboard w zaleznosci do stanu zalogowania

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,29 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import {useAuth} from "./auth/AuthContext";
 
 
 function App() {
-  return (
-    <BrowserRouter>
+  const {isAuthenticated} = useAuth();
+    return (
       <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/login" element={
+            isAuthenticated ? <Navigate to="/dashboard" />
+                :
+            <LoginPage/>
+        }/>
+          <Route path="/dashboard" element={
+              isAuthenticated ? <DashboardPage />
+                :
+                <Navigate to="/login" />
+          } />
+
+          <Route path="*" element={
+              <Navigate to="/login" />
+          }
+                 />
       </Routes>
-    </BrowserRouter>
   );
 }
 
