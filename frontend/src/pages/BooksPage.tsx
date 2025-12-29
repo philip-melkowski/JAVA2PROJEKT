e268fb0fe43d77925c252011d6ba383d1fd03b0f
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2025-12-29 21:51:54 +0100

    zastapienie htmla MUI design w booksPage

--- frontend/src/pages/BooksPage.tsx
+++ frontend/src/pages/BooksPage.tsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import {type BookDTO, getBooks} from "../api/booksApi.ts";
+import {Box, Card, Divider, Stack, Typography, CardContent} from "@mui/material";
 
 
 
@@ -21,7 +22,42 @@ export default function BooksPage() {
         }
         fetchData();
     }, []);
+
     return (
-    <div>Liczba książek: {booksList.length}</div>
+
+        <Box
+            sx = {{flexGrow: 1, maxWidth: 550}}
+        >
+        <Typography sx = {{mt: 4, mb:2 }} variant="h3" component="div">
+            Książki
+        </Typography>
+            <Stack spacing={2}
+                   divider = {<Divider orientation="horizontal" flexItem />}
+            >
+
+
+            {booksList.map(book =>
+                (
+                    <Card
+                        variant="outlined">
+                        <CardContent>
+                        <Typography
+                            variant="h5">
+                            Title: {book.title}
+                        </Typography>
+                        <Typography
+                            variant="h6">
+                            Author: {book.authorName} {book.authorSurname}
+                        </Typography>
+                        <Typography
+                            variant="h6">
+                            Average rating: {book.averageRating}
+                        </Typography>
+                        </CardContent>
+                    </Card>
+                ))}
+            </Stack>
+        </Box>
+
     )
 }
