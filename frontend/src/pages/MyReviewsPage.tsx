0ce31648bf3c371bccce8193d24848daa79420c9
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2026-01-08 13:35:39 +0100

    dodanie możliowści edycji recenzji + refetch przy edycji

--- frontend/src/pages/MyReviewsPage.tsx
+++ frontend/src/pages/MyReviewsPage.tsx
@@ -1,5 +1,5 @@
-import {Button, Dialog, Stack, Typography} from "@mui/material";
-import {getMyReviews, type ReviewDTO} from "../api/reviewApi.ts";
+import {Button, Dialog, InputLabel, MenuItem, Select, Stack, TextField, Typography} from "@mui/material";
+import {getMyReviews, type ReviewDTO, updateReview} from "../api/reviewApi.ts";
 import {useEffect, useState} from "react";
 
 
@@ -11,6 +11,17 @@ export default function MyReviewsPage() {
     const [selectedReview, setSelectedReview] = useState<ReviewDTO | null>(null);
 
     const COMMENT_PREVIEW_LENGTH = 15; // długość tekstu recenzji widocznego w tabeli
+
+    // stany do edit review
+    const [isEditDialogOpen, setIsEditDialogOpen] = useState<boolean>(false);
+    const [editedReviewId, setEditedReviewId] = useState<number | null>(null);
+    const [editedRating, setEditedRating] = useState<number | null>(null);
+    const [editedComment, setEditedComment] = useState<string | null>(null);
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+    const [editedBookId, setEditedBookId] = useState<number | null>(null);
+
+    // stan do refetch
+    const [shouldRefetch, setShouldRefetch] = useState<boolean>(true);
 
 
     useEffect(() => {
@@ -30,7 +41,7 @@ export default function MyReviewsPage() {
             }
         }
         fetchReviews();
-    }, []);
+    }, [shouldRefetch]);
 
     return <>
         {loading && <Typography variant="h6">Loading...</Typography>}
@@ -61,6 +72,67 @@ export default function MyReviewsPage() {
                     key={rev.id}
                     >
                     <Dialog
+                        open={isEditDialogOpen}
+                        onClose={() => {
+                            setIsEditDialogOpen(false);
+                            setEditedReviewId(null);
+                            setEditedRating(null);
+                            setEditedComment(null);
+                            setEditedBookId(null);} }
+
+                    >
+                        <Typography>Edit review</Typography>
+                        <InputLabel
+                            id="rating-select-label">Rating</InputLabel>
+                        <Select
+                            disabled={isSubmitting}
+                            required={true}
+                            labelId="rating-select-label"
+                            value={editedRating}
+                            onChange={(e) => setEditedRating(Number(e.target.value))}
+                        >
+                            <MenuItem value={1}>1</MenuItem>
+                            <MenuItem value={2}>2</MenuItem>
+                            <MenuItem value={3}>3</MenuItem>
+                            <MenuItem value={4}>4</MenuItem>
+                            <MenuItem value={5}>5</MenuItem>
+                            <MenuItem value={6}>6</MenuItem>
+                            <MenuItem value={7}>7</MenuItem>
+                            <MenuItem value={8}>8</MenuItem>
+                            <MenuItem value={9}>9</MenuItem>
+                            <MenuItem value={10}>10</MenuItem>
+                        </Select>
+                        <TextField
+                            id="review-field"
+                            label="Review"
+                            multiline
+                            rows={4}
+                            value={editedComment}
+                            onChange={(e) => setEditedComment(e.target.value)}
+                        ></TextField>
+                        <Stack
+                            direction="row"
+                        >
+                            <Button
+                                type="submit"
+                                onClick={ async () =>
+                                    {
+                                        setIsSubmitting(true);
+                                        await updateReview({rating: Number(editedRating), comment: editedComment, bookId: Number(editedBookId)}, editedReviewId!);
+                                        setShouldRefetch(prev => !prev);
+                                        setIsEditDialogOpen(false);
+                                        setEditedReviewId(null);
+                                        setEditedRating(null);
+                                        setEditedComment(null);
+                                        setEditedBookId(null);
+                                        setIsSubmitting(false);
+                                    }
+                                }
+                            >Edit Review!</Button>
+                            <Button onClick={() => {setIsEditDialogOpen(false); setEditedReviewId(null); setEditedRating(null); setEditedComment(null); setEditedBookId(null);}}>Cancel</Button>
+                        </Stack>
+                    </Dialog>
+                    <Dialog
                         open={selectedReview !== null}
                         onClose={() => setSelectedReview(null)}
                     ><Typography>{selectedReview?.comment}</Typography></Dialog>
@@ -81,7 +153,19 @@ export default function MyReviewsPage() {
                                 , "&:hover": {color: "primary.main"}
                     }}
                         variant="h6">{rev.comment.length-1 > COMMENT_PREVIEW_LENGTH ? rev.comment.slice(0, COMMENT_PREVIEW_LENGTH) + "..." : rev.comment}</Typography>
-                    <Button>Edit review</Button>
+                    <Button
+                        disabled={isSubmitting}
+                        onClick={
+                            () =>
+                                {
+                                    setIsEditDialogOpen(true);
+                                    setEditedReviewId(rev.id);
+                                    setEditedRating(rev.rating);
+                                    setEditedComment(rev.comment);
+                                    setEditedBookId(rev.bookId);
+                                }
+                        }
+                    >Edit review</Button>
                     <Button>Delete review</Button>
                 </Stack>
             )
